aedc8150adb4ada150f399a13ecacc04176568e8
Daniel Derevjanik <daniel.derevjanik@gmail.com>
2026-03-25 20:30:09 +0100

    recator: prefix util logs

--- src/engine/Utils.cpp
+++ src/engine/Utils.cpp
@@ -40,8 +40,8 @@ bool checkFileMD5Hash(const void* data, uint64_t numBytes, uint64_t checkHashWor
     uint64_t actualHashWord2 = {};
 
     if (getFileMD5Hash(data, numBytes, actualHashWord1, actualHashWord2)) {
-        printf("actualHashWord1 %I64X\n", actualHashWord1);
-        printf("actualHashWord2 %I64X\n", actualHashWord2);
+        printf("[Utils] actualHashWord1 %I64X\n", actualHashWord1);
+        printf("[Utils] actualHashWord2 %I64X\n", actualHashWord2);
         return ((actualHashWord1 == checkHashWord1) && (actualHashWord2 == checkHashWord2));
     }
     else {
